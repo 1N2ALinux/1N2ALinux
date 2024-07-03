9c4ede8ddb4575d434e05d8a654593d86ce019d6
1N2ALinux <163660088+1N2ALinux@users.noreply.github.com>
2024-07-03 08:53:51 +0000

    Add files via upload

---
+++ scriptContact.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuButton = document.getElementById('menuButton');
+    var menu = document.getElementById('menu');
+    var overlay = document.getElementById('overlay');
+
+    menuButton.addEventListener('click', function() {
+        menu.classList.toggle('open');
+        overlay.style.display = menu.classList.contains('open') ? 'block' : 'none';
+    });
+
+    overlay.addEventListener('click', function() {
+        menu.classList.remove('open');
+        overlay.style.display = 'none';
+    });
+
+    document.getElementById('homeButton').onclick = function() {
+        window.location.href = 'Profil.html'; // Ganti 'Profil.html' dengan URL halaman tujuan Anda
+    };
+});
---
+++ scriptHome.js
@@ -0,0 +1,17 @@
+var menuButton = document.getElementById('menuButton');
+var menu = document.getElementById('menu');
+var overlay = document.getElementById('overlay');
+
+menuButton.addEventListener('click', function() {
+    menu.classList.toggle('open');
+    overlay.style.display = menu.classList.contains('open') ? 'block' : 'none';
+});
+
+overlay.addEventListener('click', function() {
+    menu.classList.remove('open');
+    overlay.style.display = 'none';
+});
+
+document.getElementById('homeButton').onclick = function() {
+    window.location.href = 'Profil.html'; // Ganti 'YourNewPage.html' dengan URL halaman tujuan Anda
+};
